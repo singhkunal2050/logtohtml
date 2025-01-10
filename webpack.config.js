bcff8c848afa53c710cb6ed16744ba817951f687
Kunal SIngh <kunal.singh@clevertap.com>
2025-01-10 14:30:13 +0530

    Added Webpack Bundler

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js', // The entry point for bundling
+  output: {
+    filename: 'bundle.js', // Output bundled file
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader', // Optional: if you're using Babel for JS transpiling
+      },
+    ],
+  },
+  mode: 'production', // For production build
+};
